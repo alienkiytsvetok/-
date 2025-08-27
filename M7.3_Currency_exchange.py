1bdaefba972a15a7ba43a024c684900c432cb3b5
AlienkiyTsvetok <alienkiytsvetok@gmail.com>
2025-08-27 22:04:44 +0300

    Добавлена метка с названием базовой валюты.

--- M7.3_Currency_exchange.py
+++ M7.3_Currency_exchange.py
@@ -6,11 +6,18 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 
 
-def update_c_label(event):  # event - потому что возникает, когда происходит ComboboxSelected
+def update_t_label(event):  # event - потому что возникает, когда происходит ComboboxSelected
     # Получаем полное название валюты из словаря и обновляем метку
     code = t_combobox.get()
     name = cur[code]
-    c_label.config(text=name)
+    t_label.config(text=name)
+
+
+def update_b_label(event):  # event - потому что возникает, когда происходит ComboboxSelected
+    # Получаем название базовой валюты из словаря и обновляем метку
+    code = b_combobox.get()
+    name = cur[code]
+    b_label.config(text=name)
 
 
 def exchange():
@@ -59,15 +66,19 @@ window.geometry('400x300')
 Label(text="Базовая валюта").pack(padx=10, pady=10)
 b_combobox = ttk.Combobox(values=list(cur.keys()))
 b_combobox.pack(padx=10, pady=10)
+b_combobox.bind("<<ComboboxSelected>>", update_b_label)
+
+b_label = ttk.Label()
+b_label.pack(padx=10, pady=10)
 
 Label(text="Целевая валюта").pack(padx=10, pady=10)
 
 t_combobox = ttk.Combobox(values=list(cur.keys()))
 t_combobox.pack(padx=10, pady=10)
-t_combobox.bind("<<ComboboxSelected>>", update_c_label)
+t_combobox.bind("<<ComboboxSelected>>", update_t_label)
 
-c_label = ttk.Label()
-c_label.pack(padx=10, pady=10)
+t_label = ttk.Label()
+t_label.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена", command=exchange).pack(padx=10, pady=10)
 
